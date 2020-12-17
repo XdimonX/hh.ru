e3a35a9a7becccae1ac001f83f6d7a9df25a8dd1
Dimentr <xdimon777@gmail.com>
2020-12-17 16:23:29 +0400

    next

--- telegram.go
+++ telegram.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"fmt"
-	tb "gopkg.in/tucnak/telebot.v2"
+	// "fmt"
 	"time"
+
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func tst() {
 	b, err := tb.NewBot(tb.Settings{Token: token, Poller: &tb.LongPoller{Timeout: 10 * time.Second}})
 	checkErr(err)
-	_ = b
-	fmt.Println(b.Me.Username)
+	g := tb.User{ID: 385060683}
+	var (
+		menu    = &tb.ReplyMarkup{ResizeReplyKeyboard: true}
+		btnHelp = menu.Text("ℹ Help")
+	)
+	menu.Reply(
+		menu.Row(btnHelp),
+	)
+	b.Handle(&btnHelp, func(m *tb.Message) {
+		b.Send(m.Chat, "Тута будет помощь.")
+	})
+	b.Handle("/help", func(m *tb.Message) {
+		b.Send(m.Chat, "test", menu)
+	})
+	b.Send(&g, "Запуск - "+time.Now().Format(time.ANSIC))
+	b.Start()
 }
