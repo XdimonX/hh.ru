68e75ce694cffcc232b031c860602c26d2062fe1
Dimentr <xdimon777@gmail.com>
2021-01-19 14:24:03 +0400

    next

--- compile.bat
+++ compile.bat
@@ -3,4 +3,4 @@ rem set CGO_CFLAGS=-Ic:/AutoItX
 rem set CGO_LDFLAGS=-lAutoItX3_DLL
 go build -ldflags="-s -w -H=windowsgui"
 rem go build -ldflags="-H=windowsgui"
-Y:\work\Golang\test\upx.exe -9 *.exe 
+Y:\work\Golang\test\upx.exe --best *.exe 
