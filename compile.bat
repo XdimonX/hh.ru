b4095d226bd6ea427025c0ae28eab77906794b86
xdimon <xdimon777@gmail.com>
2023-06-19 16:03:04 +0400

    Переделка  проекта под горутины и переменные окружения

--- compile.bat
+++
@@ -1,7 +0,0 @@
-set GOARCH=386
-rem set CGO_CFLAGS=-Ic:/AutoItX
-rem set CGO_LDFLAGS=-lAutoItX3_DLL
-go build -ldflags="-s -w -H=windowsgui"
-rem go build -ldflags="-s -w"
-rem go build -ldflags="-H=windowsgui"
-upx.exe --best *.exe 
